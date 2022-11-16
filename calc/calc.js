f60d10006f4275a343cadee7c1a237cb7128d07c
BreadEatBread <woong4252@gmail.com>
2022-11-16 17:21:10 +0900

    11.16.22 17:20

--- calc/calc.js
+++ calc/calc.js
@@ -1,8 +1,35 @@
 var buttons = document.querySelectorAll("button");
-
-        for (var i = 0; i < buttons.length; i++) {
-            buttons[i].addEventListener("click", (event) => {
-                var text = event.target.innerText;
-                document.querySelector("input").value += text;
-            });
+var input = document.querySelector("input");
+var operand = undefined; // 값을 대입하지 않았다.
+var flag = false; // 연산버튼을 클릭했다.
+for (var i = 0; i < buttons.length; i++) {
+  // 반복 실행
+  buttons[i].addEventListener("click", (event) => {
+    // EventListner -> click 이벤트에 실행되는 코드
+    var text = event.target.innerText;
+    console.log(text);
+    switch (text) {
+      case "+":
+      case "-":
+      case "x":
+      case "/":
+        operand = parseInt(input.value);
+        flag = true;
+        break;
+      default:
+        // 연산 버튼을 눌렀을 때 수를 새로 쓴다.
+        if (flag) {
+          input.value = text;
+          flag = false;
+        } else {
+          input.value += text;
         }
+    }
+    // 1. 변수 사용
+    // var origin = input.value;
+    // input.value = origin + text;
+    // 2. 식 하나로
+    // input.value = input.value;
+    // 3. 축약 연산자. input의 value에 text를 저장 (input.value = input.value + text)
+  });
+}

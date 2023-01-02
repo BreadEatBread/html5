b90109b9a4ba1b229a90075c67755aec6347d1ab
BreadEatBread <woong4252@gmail.com>
2023-01-02 17:07:53 +0900

    01.02.23 17:20

---
+++ js_example/quiz1.js
@@ -0,0 +1,9 @@
+// const button = document.querySelector()
+// const button = document.getElementById()
+// button.onclick = () => {
+//     alert('반갑습니다.');
+// }
+
+const comealeart = () => {
+  alert("반갑습니다.");
+};
